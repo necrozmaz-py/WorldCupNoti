e175875e6f13a9e5fff71ab9c4bfe27063c290fd
Bluelime Learning <r.antia826@gmail.com>
2026-06-20 11:12:03 +0100

    Scraped

--- Main.py
+++ Main.py
@@ -1,9 +1,18 @@
-from bs4 import BeautifulSoup
 import requests
-url      = "https://www.skysports.com/football/news/11095/13481245/world-cup-2026-fixture-schedule-and-uk-kick-off-times-day-by-day-breakdown-of-all-104-matches-including-england-scotland"
-response = requests.get(url)
-soup     = BeautifulSoup(response.text, "html.parser")
-page_text = soup.get_text(separator="\n", strip=True) #
+from bs4 import BeautifulSoup
+
+Fballurl = "https://www.skysports.com/football-scores-fixtures/2026-06-21"
+response = requests.get(Fballurl)
+
+if response.status_code == 200:
+    print("Request successful!")
+else: 
+    print("Request failed with status code:", response.status_code)
+
+soup      = BeautifulSoup(response.text, "html.parser")
+page_text = soup.get_text(separator="\n", strip=True)    
 print(soup.title)
+print(page_text)
+
 
 
